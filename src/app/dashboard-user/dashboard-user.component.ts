51ef53851a306dd63f33ddc53b68c485615a2462
KennyR <58228730+LawderDev@users.noreply.github.com>
2024-06-28 00:16:15 +0200

    add modal

--- src/app/dashboard-user/dashboard-user.component.ts
+++ src/app/dashboard-user/dashboard-user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ReactiveFormsModule, FormGroup, FormControl, Validators} from '@angular/forms';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 interface Patient {
   name: string;
@@ -10,33 +12,63 @@ interface Patient {
 @Component({
   selector: 'app-dashboard-user',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './dashboard-user.component.html',
   styleUrl: './dashboard-user.component.scss'
 })
 export class DashboardUserComponent {
- //TODO replace to database
- patients: Patient[] = [
-  {
-    name: 'Cy Ganderton',
-    apointmentDate: '2022-01-01 10:00:00',
-    status: 'in_consultation'
-  },
-  {
-    name: 'Hart Hagerty',
-    apointmentDate: '2022-01-01 10:00:00',
-    status: 'waiting'
-  },
-];
+  currentDateTime: string;
+  addPatientForm: FormGroup;
+  patients: Patient[] = [];
 
-getStatus(status: string) {
-  switch (status) {
-    case 'in_consultation':
-      return 'En consultation';
-    case 'waiting':
-      return 'En attente';
-    default:
-      return 'En attente';
+  constructor() {
+    const now = new Date();
+    this.currentDateTime = now.toISOString().slice(0, 16);
+    this.addPatientForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      date: new FormControl(this.currentDateTime, [Validators.required, this.futureDateValidator]),
+    })
+    this.patients = [
+      {
+        name: 'Cy Ganderton',
+        apointmentDate: '2022-01-01 10:00:00',
+        status: 'in_consultation'
+      },
+      {
+        name: 'Hart Hagerty',
+        apointmentDate: '2022-01-01 10:00:00',
+        status: 'waiting'
+      },
+    ]
   }
-}
+
+  futureDateValidator(control: AbstractControl): ValidationErrors | null {
+    const inputDate = new Date(control.value);
+    const now = new Date();
+    return inputDate > now ? null : { futureDate: true };
+  }
+
+  getStatus(status: string) {
+    switch (status) {
+      case 'in_consultation':
+        return 'En consultation';
+      case 'waiting':
+        return 'En attente';
+      default:
+        return 'En attente';
+    }
+  }
+
+  addPatient() {
+    console.log(this.addPatientForm.value);
+    //TODO
+  }
+
+  startConsultation() {
+    //TODO
+  }
+
+  endConsultation() {
+    //TODO
+  }
 }
